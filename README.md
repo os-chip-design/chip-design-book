45851dbc5f42427f78511420db837b9965f2297f
Martin Schoeberl <martin@jopdesign.com>
2026-01-13 13:30:42 +0100

    Minor changes, link to PDF of the book

--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 This eBook is an introduction to chip design for a Bachelor level class.
 The practical labs focus on using open-source tools only.
 This enables the students to explore and tinker in chip design on their own laptops.
+
+Find a PDF of this book at [Chip Design Book](https://www.imm.dtu.dk/~masca/chip-design-book.html)
--- code/config.py
+++ code/config.py
@@ -2,9 +2,9 @@ from librelane.config import Config
 
 Config.interactive(
     "adder",
-    PDK="sky130A",
-    CLOCK_PORT="clk",
-    CLOCK_NET="clk",
-    CLOCK_PERIOD=10,
-    PRIMARY_GDSII_STREAMOUT_TOOL="klayout",
+    PDK = "sky130A",
+    CLOCK_PORT = "clock",
+    CLOCK_NET = "clock",
+    CLOCK_PERIOD = 20,
+    PRIMARY_GDSII_STREAMOUT_TOOL = "klayout",
 )
