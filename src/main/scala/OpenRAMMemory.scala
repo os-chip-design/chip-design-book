22dc724a6362a6cffb3f8285720bb0158b265438
Martin Schoeberl <martin@jopdesign.com>
2026-04-15 12:46:50 +0200

    memory: ignore timing

--- src/main/scala/OpenRAMMemory.scala
+++ src/main/scala/OpenRAMMemory.scala
@@ -8,21 +8,18 @@ class OpenRAMMemory extends Module {
         val dout  = Output(UInt(32.W))
     })
 
-
-   val counter = RegInit(0.U(32.W))
-
    val mem = Module(new SramMacro)
    mem.io.clk0 := clock
    mem.io.csb0 := false.B
    mem.io.web0 := !io.we
    mem.io.wmask0 := "b1111".U
-   mem.io.addr0 := counter
+   mem.io.addr0 := io
    mem.io.din0 := io.din
    io.dout := mem.io.dout0
 
    mem.io.clk1 := clock
    mem.io.csb1 := false.B
-   mem.io.addr1 := counter
+   mem.io.addr1 := io.addr
    // io.dout := mem.io.dout1
 }
 
