4e6fbe47b77c2af5c91efd60701c99c2af9620fc
Martin Schoeberl <martin@jopdesign.com>
2026-04-15 12:40:36 +0200

    Fix memory example

--- src/main/scala/OpenRAMMemory.scala
+++ src/main/scala/OpenRAMMemory.scala
@@ -9,19 +9,20 @@ class OpenRAMMemory extends Module {
     })
 
 
+   val counter = RegInit(0.U(32.W))
 
    val mem = Module(new SramMacro)
    mem.io.clk0 := clock
    mem.io.csb0 := false.B
    mem.io.web0 := !io.we
    mem.io.wmask0 := "b1111".U
-   mem.io.addr0 := io.addr
+   mem.io.addr0 := counter
    mem.io.din0 := io.din
    io.dout := mem.io.dout0
 
    mem.io.clk1 := clock
    mem.io.csb1 := false.B
-   mem.io.addr1 := io.addr
+   mem.io.addr1 := counter
    // io.dout := mem.io.dout1
 }
 
